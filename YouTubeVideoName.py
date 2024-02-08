d6863d74fdc91dd98c5ae4230b2ea1b99b7b607d
DawnbreakerVII <137497851+DawnbreakerVII@users.noreply.github.com>
2024-02-08 19:18:37 +0300

    Youtube video search automation
    Clicking on your own video by scrolling through a title.

---
+++ YouTubeVideoName.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+driver = webdriver.Chrome()
+
+driver.get("https://www.youtube.com/")
+driver.maximize_window()
+time.sleep(2)
+Giris= driver.find_element(By.XPATH,"//input[@id='search']")
+Giris.send_keys("Title Name")    # The title by which you want to search the video 
+Giris.send_keys(Keys.ENTER)
+time.sleep(2)                    # Adjust according to internet speed
+loopCounter = 0
+last_height = driver.execute_script("return document.documentElement.scrollHeight")
+while True:
+    if loopCounter > 7: 
+        break
+    driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")
+    time.sleep(3)
+
+    new_height = driver.execute_script("return document.documentElement.scrollHeight")
+    if last_height == new_height:  
+        break
+    last_height = new_height
+    loopCounter+=1         
+time.sleep(3)
+son = driver.find_element(By.XPATH,"//yt-formatted-string[.='Video Name']") # Enter the video name
+son.click()
+input("Press ENTER to exit\n")
